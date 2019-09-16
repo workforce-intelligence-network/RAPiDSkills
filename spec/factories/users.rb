5d820a43f2c80e4b18e0226b63a4c86076650b91
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-16 10:43:46 -0700

    Add User model #35

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    name { "MyString" }
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "has a valid factory" do
+    user = build(:user)
+    expect(user.valid?).to be true
+  end
+end
