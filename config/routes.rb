91bba4fb917607d8f63853e7d19a77dea5d6d037
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-16 14:47:59 -0700

    Add Admin Login link to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  root to: "admin/dashboard#index"
   ActiveAdmin.routes(self)
 
   resources :users, path: 'welcome', path_names: { new: '' },
     only: [:new, :create]
+
+  root to: "users#new"
 end
