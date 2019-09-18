ffb67941513529d55b82d151af25e06b5c6ebed9
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-18 12:11:10 -0700

    Use credentials file to store mailer domain #41

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rapid_skills_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: ENV["DOMAIN"] }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.domain }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
