482265ef467ac2a02c36cfec98049bec2c1dc6f3
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-18 10:21:46 -0700

    Add Mailgun sandbox domain for now #41

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'rapid-skills.heroku.com',
+    domain: 'sandbox75f08d25261e4bf6bc652fa887050338.mailgun.org',
     authentication: :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
