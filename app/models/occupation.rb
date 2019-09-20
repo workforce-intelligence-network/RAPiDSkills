0a187f3bc9956cfd8c8cac041100f0e9ce24895b
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-20 11:08:48 -0700

    Add rapids_code uniqueness constraint at app level #34

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -1,4 +1,6 @@
 class Occupation < ApplicationRecord
+  validates :rapids_code, presence: true, uniqueness: true
+
   def title_aliases=(list)
     if list.is_a?(String)
       super list.split(/,\s+/)
--- spec/models/occupation_spec.rb
+++ spec/models/occupation_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Occupation, type: :model do
     o = build(:occupation)
     expect(o.valid?).to be true
   end
+
+  it "has unique rapids_code" do
+    o = create(:occupation)
+    new_o = build(:occupation, rapids_code: o.rapids_code)
+    expect(new_o.valid?).to be false
+  end
 end
