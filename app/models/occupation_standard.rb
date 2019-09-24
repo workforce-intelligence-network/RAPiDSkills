e32bb6039cc1daf1e67f3db99789050fdf1dc65d
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-24 10:49:05 -0700

    Add to_s method for occupation standard #8

--- app/models/occupation_standard.rb
+++ app/models/occupation_standard.rb
@@ -4,4 +4,8 @@ class OccupationStandard < ApplicationRecord
   belongs_to :industry, optional: true
   belongs_to :creator, class_name: 'User'
   belongs_to :parent_occupation_standard, class_name: 'OccupationStandard', optional: true
+
+  def to_s
+    "#{occupation.title} (#{organization.title})"
+  end
 end
