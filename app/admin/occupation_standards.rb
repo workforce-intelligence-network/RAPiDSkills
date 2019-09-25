687e54b08ff2a741425284531df7c7cf8535cd24
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-25 11:57:40 -0700

    Add skills selection to occupation_standard dashboard

--- app/admin/occupation_standards.rb
+++ app/admin/occupation_standards.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register OccupationStandard do
-  permit_params :type, :organization_id, :creator_id, :occupation_id, :data_trust_approval, :parent_occupation_standard_id, :industry_id, :completed_at, :published_at, :pdf_file_url, :excel_file_url, :source_file_url
+  permit_params :type, :organization_id, :creator_id, :occupation_id, :data_trust_approval, :parent_occupation_standard_id, :industry_id, :completed_at, :published_at, :pdf_file_url, :excel_file_url, :source_file_url, skill_ids: []
 
   SUBMODELS = %w(FrameworkStandard RegisteredStandard GuidelineStandard UnregisteredStandard)
 
@@ -32,6 +32,7 @@ ActiveAdmin.register OccupationStandard do
       row :data_trust_approval
       row :parent_occupation_standard
       row :industry
+      row :skills
       row :completed_at
       row :published_at
       row :pdf_file_url
@@ -53,6 +54,7 @@ ActiveAdmin.register OccupationStandard do
       f.input :data_trust_approval
       f.input :parent_occupation_standard
       f.input :industry
+      f.input :skills, include_blank: true
       f.input :completed_at
       f.input :published_at
       f.input :pdf_file_url
