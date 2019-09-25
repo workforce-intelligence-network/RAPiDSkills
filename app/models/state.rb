2134b5295f4e0a7453b81bae569daf4d95c90b0f
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-25 14:38:39 -0700

    Add standards registration admin dashboard

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,8 @@
 class State < ApplicationRecord
   validates :short_name, presence: true, uniqueness: true
   validates :long_name, presence: true, uniqueness: true
+
+  def to_s
+    long_name
+  end
 end
