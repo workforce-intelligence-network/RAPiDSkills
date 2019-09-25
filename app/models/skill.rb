d0fff6de25e61b415c5398c57bd4f4694628cd18
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-25 15:43:41 -0700

    Add associations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,8 @@
 class Skill < ApplicationRecord
-  belongs_to :work_process
+  belongs_to :work_process, optional: true
   belongs_to :parent_skill, class_name: 'Skill', optional: true
+  has_many :occupation_standard_skills
+  has_many :occupation_standards, through: :occupation_standard_skills
 
   def to_s
     description
--- app/models/state.rb
+++ app/models/state.rb
@@ -2,6 +2,10 @@ class State < ApplicationRecord
   validates :short_name, presence: true, uniqueness: true
   validates :long_name, presence: true, uniqueness: true
 
+  has_many :locations
+  has_many :organizations, through: :locations
+  has_many :standard_registrations
+
   def to_s
     long_name
   end
