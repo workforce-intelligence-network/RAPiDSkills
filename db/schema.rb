85ed27253a4ec7817a08d947b67d385bc35f856e
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-09-25 12:51:42 -0700

    Add after_party db table and config file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_194122) do
+ActiveRecord::Schema.define(version: 2019_09_25_195045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,10 @@ ActiveRecord::Schema.define(version: 2019_09_25_194122) do
     t.string "long_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "task_records", id: false, force: :cascade do |t|
+    t.string "version", null: false
   end
 
   create_table "users", force: :cascade do |t|
