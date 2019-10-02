1c89a7ec0bf8886c361b6806bf64fc90c30acfd5
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-02 15:15:00 -0700

    Add active storage tables

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,27 @@ ActiveRecord::Schema.define(version: 2019_10_09_233556) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "industries", force: :cascade do |t|
     t.string "title"
     t.string "naics_code"
@@ -178,6 +199,7 @@ ActiveRecord::Schema.define(version: 2019_10_09_233556) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "locations", "organizations"
   add_foreign_key "locations", "states"
   add_foreign_key "occupation_standard_skills", "occupation_standards"
