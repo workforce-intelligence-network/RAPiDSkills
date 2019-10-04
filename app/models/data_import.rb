23c19c4206bb8dc90b4f96597081f5ba5f5c57a2
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-03 17:21:30 -0700

    Stub importing file after file change #51

--- app/models/data_import.rb
+++ app/models/data_import.rb
@@ -1,5 +1,22 @@
 class DataImport < ApplicationRecord
+  include Redis::Objects
+
   has_one_attached :file
 
   enum kind: [:occupation_standards]
+
+  value :blob_key
+
+  after_save :run_import, if: :file_changed?
+
+  private
+
+  def file_changed?
+    blob_key.value != file.blob.key
+  end
+
+  def run_import
+    # Import data here according to kind
+    self.blob_key = file.blob.key
+  end
 end
