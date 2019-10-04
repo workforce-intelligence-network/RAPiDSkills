ba1ecb33ef134b96547bccb2f57f3f1b4f7b4560
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-04 12:26:55 -0700

    Use file fixture in data_import factory #51

--- spec/factories/data_imports.rb
+++ spec/factories/data_imports.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :data_import do
+    file { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'test.csv'), 'text/csv') }
     description { Faker::Lorem.sentence }
     kind { 0 }
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,11 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
