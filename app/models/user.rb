736bc6ac304f458681a80905de85f37dac9256de
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-09 15:59:36 -0700

    Remove confirmable for now #54

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :confirmable, :trackable,
+  devise :database_authenticatable, :trackable,
          :recoverable, :rememberable, :validatable
 
   enum role: [:basic, :admin]
