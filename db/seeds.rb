2c8d295cea7c765d7346ad0337d566c762dd66bc
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-09 15:57:05 -0700

    Setup ActiveAdmin to use with User model #54

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: :admin) if Rails.env.development?
 
 Rake::Task['after_party:run'].invoke
