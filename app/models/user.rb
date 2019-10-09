b6fa675ae6a2b491ff870e4a31943e3a84b42d07
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-09 15:40:17 -0700

    Add Devise to users model #54

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :confirmable, :trackable,
+         :recoverable, :rememberable, :validatable
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
+    password { "supersecret" }
     name { "MyString" }
   end
 end
