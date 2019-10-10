386ed7d89ea075a707e304094e0fa6a2901f0c21
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-10 13:55:09 -0700

    Require fields on work_processes, skills #51

--- app/models/work_process.rb
+++ app/models/work_process.rb
@@ -2,4 +2,6 @@ class WorkProcess < ApplicationRecord
   has_many :skills
   has_many :occupation_standard_work_processes
   has_many :occupation_standards, through: :occupation_standard_work_processes
+
+  validates :title, :description, :hours, presence: true
 end
