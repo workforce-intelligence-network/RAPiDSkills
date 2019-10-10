1fdc93715bbc2011f8bf1af6447544940bbe5202
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-10 16:48:15 -0700

    Remove admin_user model spec #51

--- spec/factories/admin_users.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :admin_user do
-    sequence(:email) { |n| "admin#{n}@example.com" }
-    password { "supersecret" }
-  end
-end
--- spec/models/admin_user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AdminUser, type: :model do
-  it "has a valid factory" do
-    admin = build(:admin_user)
-    expect(admin.valid?).to be true
-  end
-end
