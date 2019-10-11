d31060b857dd8f29fe8cc7682a7739e123be4f80
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-10 17:04:04 -0700

    Fix occupation_standard factory #51

--- spec/factories/occupation_standards.rb
+++ spec/factories/occupation_standards.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :occupation_standard do
     type { "FrameworkStandard" }
+    title { Faker::Job.title }
     organization
     association :creator, factory: :user
     occupation
