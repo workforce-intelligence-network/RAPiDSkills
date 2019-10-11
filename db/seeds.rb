be9d34d535a1d7efe89949ad39b7927af439943c
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-10 21:15:11 -0700

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: :admin) if Rails.env.development?
+User.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: :admin)
 
+Rake::Task['occupations:import'].invoke
 Rake::Task['after_party:run'].invoke
