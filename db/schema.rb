0d8e7b9925c0ab81ed13c720f97e1406edc46875
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-11 16:15:47 -0700

    Remove hours from work_processes table #51

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_11_230411) do
+ActiveRecord::Schema.define(version: 2019_10_11_231501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,7 +205,6 @@ ActiveRecord::Schema.define(version: 2019_10_11_230411) do
   create_table "work_processes", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "hours"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
