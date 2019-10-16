38f817629232779bfc5d6a9395dc55e0ecb608cd
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-16 13:43:35 -0700

    Update master skill pdf #13

--- app/models/occupation_standard.rb
+++ app/models/occupation_standard.rb
@@ -4,9 +4,9 @@ class OccupationStandard < ApplicationRecord
   belongs_to :industry, optional: true
   belongs_to :creator, class_name: 'User'
   belongs_to :parent_occupation_standard, class_name: 'OccupationStandard', optional: true
-  has_many :occupation_standard_skills
+  has_many :occupation_standard_skills, -> { order(:sort_order) }
   has_many :skills, through: :occupation_standard_skills
-  has_many :occupation_standard_work_processes
+  has_many :occupation_standard_work_processes, -> { order(:sort_order) }
   has_many :work_processes, through: :occupation_standard_work_processes
   has_many :standards_registrations
 
@@ -32,6 +32,22 @@ class OccupationStandard < ApplicationRecord
       errors.add(:base, e.message)
       OccupationStandard.new
     end
+  end
+
+  def organization_title
+    organization.title
+  end
+
+  def occupation_title
+    occupation.title
+  end
+
+  def industry_title
+    industry.title
+  end
+
+  def creator_name
+    creator.name
   end
 
   def to_s
