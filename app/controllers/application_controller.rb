a55a22ea699427242cc67f09f3d9c5358cbb42d0
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-16 12:26:36 -0700

    Direct admins to admin pages after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   def authenticate_admin_user!
     redirect_to root_path unless current_user&.admin?
   end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    resource.admin? ? admin_root_path : super
+  end
 end
