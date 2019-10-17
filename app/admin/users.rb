cd57a7392501b7c3ba8ba59144033735e3ccd2f4
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-17 15:10:38 -0700

    Add separate change password page for users on admin dashboard #57

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,20 @@
 ActiveAdmin.register User do
   permit_params :email, :name, :role, :password, :password_confirmation
 
+  action_item :change_password, only: [:edit, :show] do
+    link_to 'Change Password', change_password_admin_user_path(user)
+  end
+
+  member_action :change_password, method: [:get, :put] do
+    if request.put?
+      password_params = params.require(:user).permit(:password, :password_confirmation)
+      if resource.update_attributes(password_params)
+        flash[:success] = "You have changed the password for #{resource.email}."
+        redirect_to admin_user_path(resource)
+      end
+    end
+  end
+
   index do
     selectable_column
     id_column
@@ -25,8 +39,6 @@ ActiveAdmin.register User do
       f.input :name
       f.input :email
       f.input :role, include_blank: false
-      f.input :password
-      f.input :password_confirmation
     end
     f.actions
   end
