fcbc7a477463fa672149907754afa24e3d849c66
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-17 21:39:50 -0700

    Nest skills under work processes in pdf #63

--- app/models/occupation_standard_work_process.rb
+++ app/models/occupation_standard_work_process.rb
@@ -1,6 +1,8 @@
 class OccupationStandardWorkProcess < ApplicationRecord
   belongs_to :occupation_standard
   belongs_to :work_process
+  has_many :occupation_standard_skills
+  has_many :skills, through: :occupation_standard_skills
 
   validates :occupation_standard, uniqueness: { scope: :work_process }
   validates :hours, presence: true
