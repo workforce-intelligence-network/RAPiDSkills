b9e0fdc6adad319b3f2d0d82e838b99eaf3996cc
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-24 15:38:15 -0700

    Add occupation search method #6

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -4,6 +4,21 @@ class Occupation < ApplicationRecord
   has_many :occupation_standards
   has_many :industries, through: :occupation_standards
 
+  class << self
+    def search(q)
+      return all if q.blank?
+
+      Occupation
+        .select("O.*")
+        .from("occupations O
+                 LEFT JOIN (SELECT
+                              occupations.id,
+                              UNNEST(title_aliases) AS str
+                            FROM occupations) AS sub ON (O.id = sub.id)")
+        .where("O.title ILIKE :q OR sub.str ILIKE :q", q: "%#{q}%")
+    end
+  end
+
   def title_aliases=(list)
     if list.is_a?(String)
       super list.split(/;\s+/)
--- spec/models/occupation_spec.rb
+++ spec/models/occupation_spec.rb
@@ -17,4 +17,34 @@ RSpec.describe Occupation, type: :model do
     os = create(:occupation_standard, industry: industry)
     expect(os.occupation.industries).to eq [industry]
   end
+
+  describe ".search" do
+    let!(:occupation1) { create(:occupation, title: "Foo Bar") }
+    let!(:occupation2) { create(:occupation, title: "Bar Baz") }
+    let!(:occupation3) { create(:occupation, title: "Moo Bar", title_aliases: ["Mar", "Foo"]) }
+
+    it "returns all results when q is blank" do
+      results = Occupation.search(nil)
+      expect(results).to contain_exactly occupation1, occupation2, occupation3
+
+      results = Occupation.search("")
+      expect(results).to contain_exactly occupation1, occupation2, occupation3
+    end
+
+    it "returns matched results when q is not blank" do
+      results = Occupation.search("Foo")
+      expect(results).to contain_exactly occupation1, occupation3
+
+      results = Occupation.search("Mar")
+      expect(results).to contain_exactly occupation3
+
+      results = Occupation.search("Baz")
+      expect(results).to contain_exactly occupation2
+    end
+
+    it "returns no results when q does not match" do
+      results = Occupation.search("Fob")
+      expect(results).to be_empty
+    end
+  end
 end
