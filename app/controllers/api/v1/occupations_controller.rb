d8afa39390438d1d3f62d47bcd92db71aaa00466
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-10-30 17:36:10 -0700

    Use strong params for occupation endpoint #6

--- app/controllers/api/v1/occupations_controller.rb
+++ app/controllers/api/v1/occupations_controller.rb
@@ -1,6 +1,12 @@
 class API::V1::OccupationsController < ApplicationController
   def index
-    @occupations = Occupation.search(q: params[:q]).order(id: :desc)
+    @occupations = Occupation.search(search_params.to_h).order(id: :desc)
     render json: API::V1::OccupationSerializer.new(@occupations)
+  end
+
+  private
+
+  def search_params
+    params.permit(:q)
   end
 end
