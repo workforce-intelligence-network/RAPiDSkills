1d8f0a7cf25bb828d105e62796adfbb2801917b5
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-11-01 12:24:24 -0700

    Set up doc routes, sinatra file to render docs #68

---
+++ app/lib/docs/api.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+
+module Docs
+  class API < Sinatra::Base
+
+    get(/.+/) do
+      send_sinatra_file(request.path) {404}
+    end
+
+    not_found do
+      send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {status: 404})
+    end
+
+    def send_sinatra_file(path, &missing_file_block)
+      file_path = File.join(File.dirname(__FILE__), 'public',  path)
+      file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+      File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+    end
+  end
+end
