9c1b358acdcfaa068bad7ef9aee3f457211880e5
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-11-13 16:11:25 -0800

    Do not regenerate PDF if occupation standard has not been updated #1

--- app/jobs/generate_occupation_standard_pdf_job.rb
+++ app/jobs/generate_occupation_standard_pdf_job.rb
@@ -3,7 +3,9 @@ class GenerateOccupationStandardPdfJob < ApplicationJob
 
   def perform(occupation_standard_id)
     os = OccupationStandard.find(occupation_standard_id)
-    pdf = ::OccupationStandardPdf.new(os)
-    os.pdf.attach(io: StringIO.new(pdf.render), filename: "#{os.title.parameterize(separator: '_')}_#{I18n.l(Time.current, format: :filename)}.pdf")
+    if !os.pdf.attached? || os.pdf.created_at < os.updated_at
+      pdf = ::OccupationStandardPdf.new(os)
+      os.pdf.attach(io: StringIO.new(pdf.render), filename: "#{os.title.parameterize(separator: '_')}_#{I18n.l(Time.current, format: :filename)}.pdf")
+    end
   end
 end
--- spec/jobs/generate_occupation_standard_pdf_job_spec.rb
+++ spec/jobs/generate_occupation_standard_pdf_job_spec.rb
@@ -1,14 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe GenerateOccupationStandardPdfJob, type: :job do
-  let(:os) { create(:occupation_standard) }
-
   describe "#perform" do
-    it "calls pdf writer and attaches file" do
-      expect(OccupationStandardPdf).to receive(:new).and_call_original
-      described_class.new.perform(os.id)
-      os.reload
-      expect(os.pdf.attached?).to be true
+    shared_examples "generates pdf" do
+      it "calls pdf writer and attaches file" do
+        expect(OccupationStandardPdf).to receive(:new).and_call_original
+        described_class.new.perform(os.id)
+        os.reload
+        expect(os.pdf.attached?).to be true
+      end
+    end
+
+    context "when os does not have pdf attached" do
+      it_behaves_like "generates pdf" do
+        let(:os) { create(:occupation_standard) }
+      end
+    end
+
+    context "when os has pdf attached but it is out of date" do
+      it_behaves_like "generates pdf" do
+        let(:os) { create(:occupation_standard, :with_pdf) }
+        before { os.update!(title: "new title") }
+      end
+    end
+
+    context "when os has pdf attached but it is up-to-date" do
+      before { Timecop.freeze(time) }
+      after { Timecop.return }
+      let(:time) { Time.new(2019,11,13) }
+      let!(:os) { create(:occupation_standard) }
+
+      it "does not call pdf writer and does not attach new file" do
+        os.pdf.attach(io: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'pixel1x1.pdf'), 'application/pdf'), filename: 'file.pdf', content_type: 'application/pdf')
+        os.update_columns(updated_at: time - 1.hour)
+        pdf = os.pdf
+        expect(OccupationStandardPdf).to_not receive(:new)
+        described_class.new.perform(os.id)
+        os.reload
+        expect(os.pdf.attached?).to be true
+        expect(os.pdf).to eq pdf
+      end
     end
   end
 end
