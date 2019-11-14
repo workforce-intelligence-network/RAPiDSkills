65fa5f4732616b711742094c9da305c5c1f3b4ec
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-11-13 16:36:55 -0800

    Refactor generate occupation standard pdf job #1

--- app/jobs/generate_occupation_standard_pdf_job.rb
+++ app/jobs/generate_occupation_standard_pdf_job.rb
@@ -3,7 +3,7 @@ class GenerateOccupationStandardPdfJob < ApplicationJob
 
   def perform(occupation_standard_id)
     os = OccupationStandard.find(occupation_standard_id)
-    if !os.pdf.attached? || os.pdf.created_at < os.updated_at
+    if os.should_generate_pdf?
       pdf = ::OccupationStandardPdf.new(os)
       os.pdf.attach(io: StringIO.new(pdf.render), filename: "#{os.title.parameterize(separator: '_')}_#{I18n.l(Time.current, format: :filename)}.pdf")
     end
--- spec/jobs/generate_occupation_standard_pdf_job_spec.rb
+++ spec/jobs/generate_occupation_standard_pdf_job_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.describe GenerateOccupationStandardPdfJob, type: :job do
   describe "#perform" do
-    shared_examples "generates pdf" do
+    before do
+      allow(OccupationStandard).to receive(:find).with(os.id).and_return(os)
+    end
+
+    context "when os should generate pdf" do
+      let(:os) { create(:occupation_standard) }
+
+      before do
+        allow(os).to receive(:should_generate_pdf?).and_return(true)
+      end
+
       it "calls pdf writer and attaches file" do
         expect(OccupationStandardPdf).to receive(:new).and_call_original
         described_class.new.perform(os.id)
@@ -11,34 +21,18 @@ RSpec.describe GenerateOccupationStandardPdfJob, type: :job do
       end
     end
 
-    context "when os does not have pdf attached" do
-      it_behaves_like "generates pdf" do
-        let(:os) { create(:occupation_standard) }
+    context "when os should not generate pdf" do
+      let(:os) { create(:occupation_standard) }
+
+      before do
+        allow(os).to receive(:should_generate_pdf?).and_return(false)
       end
-    end
 
-    context "when os has pdf attached but it is out of date" do
-      it_behaves_like "generates pdf" do
-        let(:os) { create(:occupation_standard, :with_pdf) }
-        before { os.update!(title: "new title") }
-      end
-    end
-
-    context "when os has pdf attached but it is up-to-date" do
-      before { Timecop.freeze(time) }
-      after { Timecop.return }
-      let(:time) { Time.new(2019,11,13) }
-      let!(:os) { create(:occupation_standard) }
-
-      it "does not call pdf writer and does not attach new file" do
-        os.pdf.attach(io: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'pixel1x1.pdf'), 'application/pdf'), filename: 'file.pdf', content_type: 'application/pdf')
-        os.update_columns(updated_at: time - 1.hour)
-        pdf = os.pdf
+      it "does not calls pdf writer" do
         expect(OccupationStandardPdf).to_not receive(:new)
         described_class.new.perform(os.id)
         os.reload
-        expect(os.pdf.attached?).to be true
-        expect(os.pdf).to eq pdf
+        expect(os.pdf.attached?).to be false
       end
     end
   end
