f2486cc174e8952e6209beec26d1b1d38dbca3ad
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-11-14 12:46:00 -0800

    Add background job to generate csv file for occupation standard #2

---
+++ spec/jobs/generate_occupation_standard_excel_job_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe GenerateOccupationStandardExcelJob, type: :job do
+  let(:os) { create(:occupation_standard) }
+  let!(:oswp) { create(:occupation_standard_work_process, occupation_standard: os) }
+
+  describe "#perform" do
+    before do
+      allow(OccupationStandard).to receive(:find).with(os.id).and_return(os)
+    end
+
+    context "when os should generate excel" do
+      let(:os) { create(:occupation_standard) }
+
+      before do
+        allow(os).to receive(:should_generate_excel?).and_return(true)
+      end
+
+      it "attaches file" do
+        described_class.new.perform(os.id)
+        os.reload
+        expect(os.excel.attached?).to be true
+      end
+    end
+
+    context "when os should not generate excel" do
+      let(:os) { create(:occupation_standard) }
+
+      before do
+        allow(os).to receive(:should_generate_excel?).and_return(false)
+      end
+
+      it "does not attach file" do
+        described_class.new.perform(os.id)
+        os.reload
+        expect(os.excel.attached?).to be false
+      end
+    end
+  end
+end
