6977e97d4e7f8f9036d23419b2f235125f9314a2
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-12-13 15:15:25 -0800

    Add create_api_access_token method for client session

--- app/models/client_session.rb
+++ app/models/client_session.rb
@@ -1,3 +1,17 @@
 class ClientSession < ApplicationRecord
   belongs_to :user
+
+  def create_api_access_token!
+    JsonWebToken.encode(authentication_payload)
+  end
+
+  private
+
+  def authentication_payload
+    {
+      id: user_id,
+      encrypted_password: user.encrypted_password,
+      session_identifier: identifier,
+    }
+  end
 end
--- spec/models/client_session_spec.rb
+++ spec/models/client_session_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe ClientSession, type: :model do
     client_session = build(:client_session)
     expect(client_session.valid?).to be true
   end
+
+  describe "#create_api_access_token!" do
+    let(:client_session) { create(:client_session) }
+    let(:user) { client_session.user }
+
+    before do
+      allow(JsonWebToken).to receive(:encode).with(id: user.id, encrypted_password: user.encrypted_password, session_identifier: client_session.identifier).and_return("jwt123")
+    end
+
+    it "returns jwt token" do
+      jwt = client_session.create_api_access_token!
+      expect(jwt).to eq "jwt123"
+    end
+  end
 end
