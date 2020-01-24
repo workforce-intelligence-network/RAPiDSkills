74dee904f26f528ba8bb051d3e49bfbc26b99528
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-01-24 13:55:53 -0800

    Allow filtering by creator when viewing favorites #94

--- app/controllers/api/v1/users/favorites_controller.rb
+++ app/controllers/api/v1/users/favorites_controller.rb
@@ -3,7 +3,14 @@ class API::V1::Users::FavoritesController < API::V1::Users::BaseController
   def index
     authorize [:api, :v1, @target_user], :favorite?
     @favorites = @target_user.favorites.with_eager_loading
-    options = { links: { self: api_v1_user_favorites_url(@target_user) } }
+                   .search(filter_params.to_h)
+    options = { links: { self: request.original_url } }
     render json: API::V1::OccupationStandardSerializer.new(@favorites, options)
+  end
+
+  private
+
+  def filter_params
+    params.fetch(:filter, {}).permit(:creator)
   end
 end
--- spec/requests/api/v1/users/favorites_spec.rb
+++ spec/requests/api/v1/users/favorites_spec.rb
@@ -38,6 +38,30 @@ RSpec.describe API::V1::Users::FavoritesController, type: :request do
       end
     end
 
+    context "when user viewing own favorites with filter" do
+      let(:path) { api_v1_user_favorites_url(user, filter: { creator: user.id }) }
+      let(:os1) { create(:occupation_standard) }
+      let(:os2) { create(:occupation_standard, creator: user) }
+
+      before { user.favorites << os1 << os2 }
+
+      it_behaves_like "authorization", :get
+
+      it "returns favorites by filtered creator" do
+        get path, headers: header
+        expect(response).to have_http_status(:success)
+        expect(json["links"]["self"]).to eq api_v1_user_favorites_url(user, filter: { creator: user.id})
+        expect(json["data"].count).to eq 1
+        expect(json["data"][0]["type"]).to eq "occupation_standard"
+        expect(json["data"][0]["id"]).to eq os2.id.to_s
+        expect(json["data"][0]["attributes"]["title"]).to eq os2.title
+        expect(json["data"][0]["attributes"]["organization_title"]).to eq os2.organization.title
+        expect(json["data"][0]["attributes"]["occupation_title"]).to eq os2.occupation.title
+        expect(json["data"][0]["attributes"]["industry_title"]).to be nil
+        expect(json["data"][0]["links"]["self"]).to eq api_v1_occupation_standard_url(os2)
+      end
+    end
+
     context "when user viewing someone else's favorites" do
       it_behaves_like "unauthorized", :get do
         let(:header) { auth_header(create(:user)) }
