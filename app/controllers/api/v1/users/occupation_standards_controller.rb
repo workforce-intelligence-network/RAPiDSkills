4feb6d9d6a81c476ffc5c8b5461ecafd81cba595
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-01-24 14:16:56 -0800

    Use original_url for self link for user occupation standards #94

--- app/controllers/api/v1/users/occupation_standards_controller.rb
+++ app/controllers/api/v1/users/occupation_standards_controller.rb
@@ -3,9 +3,7 @@ class API::V1::Users::OccupationStandardsController < API::V1::Users::BaseContro
   def index
     authorize [:api, :v1, @target_user], :occupation_standard?
     @occupation_standards = @target_user.occupation_standards.order(id: :desc).with_eager_loading
-    options = {
-      links: { self: api_v1_user_occupation_standards_url(@target_user) }
-    }
+    options = { links: { self: request.original_url  } }
     render json: API::V1::OccupationStandardSerializer.new(@occupation_standards, options)
   end
 end
