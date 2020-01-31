b90b14279c76c1429921b758e661ffdf40a8e583
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-01-31 13:02:02 -0800

    Allow patch and delete for CORS policy

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module RapidSkills
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
       end
     end
 
