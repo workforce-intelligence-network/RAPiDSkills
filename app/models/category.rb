ca8e2af13ebeb9e4fd47b7e799b1c4e1693c3f86
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-05 12:40:16 -0800

    Add category model #117

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ApplicationRecord
+  belongs_to :occupation_standard_work_process
+
+  validates :name, presence: true,
+    uniqueness: { scope: :occupation_standard_work_process }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2020_02_15_010246) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "occupation_standard_work_process_id", null: false
+    t.integer "sort_order"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["occupation_standard_work_process_id"], name: "index_categories_on_occupation_standard_work_process_id"
+  end
+
   create_table "client_sessions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -232,6 +241,7 @@ ActiveRecord::Schema.define(version: 2020_02_15_010246) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "occupation_standard_work_processes"
   add_foreign_key "client_sessions", "users"
   add_foreign_key "data_imports", "users"
   add_foreign_key "locations", "organizations"
