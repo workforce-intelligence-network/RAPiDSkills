447cf450e2e9103abb047dead439e4138c907a66
Patrick Cushing <patrick.j.cushing@gmail.com>
2020-02-12 22:10:55 -0800

    Resolves broken spec from invalid clone

--- app/controllers/api/v1/occupation_standards_controller.rb
+++ app/controllers/api/v1/occupation_standards_controller.rb
@@ -30,12 +30,7 @@ class API::V1::OccupationStandardsController < API::V1::APIController
         organization_id: current_user.employer_id,
         new_title: create_params[:title],
       )
-      @os = OccupationStandard.includes(organization: :logo_attachment).find_by(id: @os.id)
-      if @os.valid?
-        render_resource(@os)
-      else
-        render_resource_error(@os)
-      end
+      @os.valid? ? render_resource(@os) : render_resource_error(@os)
     else
       @os = OccupationStandard.new
       @os.errors.add(:parent_occupation_standard_id, :invalid)
