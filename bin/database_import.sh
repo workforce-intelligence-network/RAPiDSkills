987a5eeb4709ab8749c98a5c15b0db393b3d36a6
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-14 11:33:37 -0800

    Add prod database import script #179

---
+++ bin/database_import.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# This script copies the production database and imports it into the local
+# development database.
+
+heroku pg:backups:capture -a rapid-skills
+heroku pg:backups:download -a rapid-skills
+docker cp latest.dump rapidskills_database_1:.
+docker-compose exec -T database bash -c 'PGPASSWORD=$POSTGRES_PASSWORD pg_restore --verbose --clean --no-acl --no-owner -w -h database -Upostgres -d rapidskills_development latest.dump && rm latest.dump'
+rm latest.dump
