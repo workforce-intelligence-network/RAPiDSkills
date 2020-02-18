26070138bd73fd2c6dd81a88b7cf6e9ac9b04e9f
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-18 14:48:51 -0800

    Revert "Tmp commit to update rapids/onet codes"
    This reverts commit e2e4e01b5f7ee5f83f1a653c99cd1127aa0ff1d1.

--- app/services/api/v1/import_occupation_standard.rb
+++ app/services/api/v1/import_occupation_standard.rb
@@ -65,16 +65,7 @@ class API::V1::ImportOccupationStandard
                else
                  raise "Either work process or skill is required"
                end
-       # occupation = Occupation.where("LOWER(title) = ?", row["occupation_standard_title"].downcase).first_or_create!(
-       #   title: row["occupation_standard_title"],
-       #   type: type,
-       #   rapids_code: row["rapids_code"],
-       #   onet_code: row["onet_code"],
-       # )
-
-        # Tmp fix to re-upload files to set rapids/onet codes
-        occupation = Occupation.where("LOWER(title) = ?", row["occupation_standard_title"].downcase).first_or_initialize
-        occupation.update!(
+        occupation = Occupation.where("LOWER(title) = ?", row["occupation_standard_title"].downcase).first_or_create!(
           title: row["occupation_standard_title"],
           type: type,
           rapids_code: row["rapids_code"],
