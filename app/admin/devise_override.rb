c62b598bfef8e8ea8d7e2b94721a208407dabfb9
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-19 08:49:48 -0800

    Return user to admin sign in after logging out on dev

--- app/admin/devise_override.rb
+++ app/admin/devise_override.rb
@@ -1,5 +1,9 @@
 ActiveAdmin::Devise::SessionsController.class_eval do
   def after_sign_out_path_for(resource_or_scope)
-    "/signout"
+    if Rails.env.development?
+      new_user_session_path
+    else
+      "/signout"
+    end
   end
 end
