49479cab7a1fcc7a1640b13a81426da34e53d7f8
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-20 11:17:24 -0800

    Customize show view for admin categories #117

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -6,4 +6,19 @@ ActiveAdmin.register Category do
   preserve_default_filters!
   remove_filter :occupation_standard_skills
   remove_filter :occupation_standard_work_process
+
+  show do |category|
+    attributes_table do
+      row :id
+      row :name
+      row :occupation_standard_work_process
+      row :sort_order
+      row :skills do |category|
+        category.occupation_standard_skills.includes(:skill).map{|oss| link_to(oss.skill.description, admin_occupation_standard_skill_path(oss)) }.join(", ").html_safe
+      end
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
 end
