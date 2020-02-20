ec4e5024d188161ea35d2883e0f2ff1bc89e1300
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-20 11:16:24 -0800

    Remove some category filters, add includes statement #117

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Category do
+  includes :occupation_standard_skills, occupation_standard_work_process: [:work_process, occupation_standard: :organization]
+
   permit_params :name, :occupation_standard_work_process_id, :sort_order
+
+  preserve_default_filters!
+  remove_filter :occupation_standard_skills
+  remove_filter :occupation_standard_work_process
 end
