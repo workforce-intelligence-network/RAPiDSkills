a1a193f2419468b7f145d4e7fb26d3f47ae6f58d
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-20 12:23:34 -0800

    Spec fixes #117

--- spec/requests/admin/data_imports_spec.rb
+++ spec/requests/admin/data_imports_spec.rb
@@ -139,8 +139,8 @@ RSpec.describe "Admin::DataImports", type: :request do
           end
 
           it "triggers pdf/excel generation" do
-            expect(GenerateOccupationStandardPdfJob).to receive(:perform_later).exactly(3).times
-            expect(GenerateOccupationStandardExcelJob).to receive(:perform_later).exactly(3).times
+            expect(GenerateOccupationStandardPdfJob).to receive(:perform_later).exactly(4).times
+            expect(GenerateOccupationStandardExcelJob).to receive(:perform_later).exactly(4).times
             post path, params: params
           end
         end
