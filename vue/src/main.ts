3f1bb3653752b4ad4360b5f940111f77c9c59783
Jake Hash <1508098+jhash@users.noreply.github.com>
2020-02-24 13:41:10 -0500

    #227 - using vue-analytics module to load ga

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 
 import '@/utilities/font-awesome';
 
@@ -9,7 +10,12 @@ import '@/registerServiceWorker';
 import router from '@/router';
 import store from '@/store';
 
+
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'GTM-TVS9FH9',
+});
 
 (async () => {
   // Make sure session is loaded before loading views
