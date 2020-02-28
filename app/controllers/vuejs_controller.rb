04187f374dcb359eb24b8bd7b0afd9cc7abf47c1
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-28 13:28:53 -0800

    Switch back to file read

--- app/controllers/vuejs_controller.rb
+++ app/controllers/vuejs_controller.rb
@@ -2,6 +2,6 @@ class VuejsController < ApplicationController
   layout false
 
   def index
-    render text: File.read("#{Rails.root}/vue/dist/index.html")
+    render file: "#{Rails.root}/vue/dist/index.html"
   end
 end
