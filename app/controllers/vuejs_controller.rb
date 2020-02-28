34b233dd68807667e62f9fd8912e4633b4b09790
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-02-28 13:34:37 -0800

    Test with cache busting just to see if that works

--- app/controllers/vuejs_controller.rb
+++ app/controllers/vuejs_controller.rb
@@ -2,6 +2,6 @@ class VuejsController < ApplicationController
   layout false
 
   def index
-    render file: "#{Rails.root}/vue/dist/index.html"
+    render file: "#{Rails.root}/vue/dist/index.html?t=#{Time.current.to_i}"
   end
 end
