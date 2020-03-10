29f55c30f0aa9704bd8db7e4b0fccd200c55a54e
Jake Hash <1508098+jhash@users.noreply.github.com>
2020-03-10 17:38:14 -0400

    #241 - using RAILS_ENV before NODE_ENV

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,6 @@
 process.env.VUE_APP_ROLLBAR_ENVIRONMENT = process.env.HEROKU_APP_NAME
   || process.env.RAPID_SKILLS_ENVIRONMENT
+  || process.env.RAILS_ENV
   || process.env.NODE_ENV
   || 'production';
 
