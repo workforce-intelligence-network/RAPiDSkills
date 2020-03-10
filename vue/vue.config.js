c8353fd6c35e5dfc67890d1184c287a779c71235
Jake Hash <1508098+jhash@users.noreply.github.com>
2020-03-10 17:24:06 -0400

    #241 - not trying to parse rapid skills env as object

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,13 +1,5 @@
-let RAPID_SKILLS_ENVIRONMENT = {};
-
-try {
-  RAPID_SKILLS_ENVIRONMENT = JSON.parse(process.env.RAPID_SKILLS_ENVIRONMENT);
-} catch {
-  //
-}
-
 process.env.VUE_APP_ROLLBAR_ENVIRONMENT = process.env.HEROKU_APP_NAME
-  || (RAPID_SKILLS_ENVIRONMENT || {}).presence // TODO: is this right?
+  || process.env.RAPID_SKILLS_ENVIRONMENT
   || process.env.NODE_ENV
   || 'production';
 
