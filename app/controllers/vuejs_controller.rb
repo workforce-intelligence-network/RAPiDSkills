8e9cd389458dd7c40000e00a5e88be98de2ad80e
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-03-10 14:44:23 -0700

    Set no-cache headers for index.html page

--- app/controllers/vuejs_controller.rb
+++ app/controllers/vuejs_controller.rb
@@ -2,6 +2,9 @@ class VuejsController < ApplicationController
   layout false
 
   def index
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
     render file: "#{Rails.root}/vue/dist/index.html"
   end
 end
