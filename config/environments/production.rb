27064f5300a71fca818d5cfd90fe24926d20759f
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-03-11 12:32:12 -0700

    Force https protocol on production #279

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   # Serializer routes
-  routes.default_url_options = { host: host }
+  routes.default_url_options = { host: host, protocol: 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
