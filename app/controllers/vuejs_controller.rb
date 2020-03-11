3e1f912e71d70d2ad460308da27d8eb09080c8af
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-03-11 10:19:35 -0700

    Add private, max-age; tmp copy change

--- app/controllers/vuejs_controller.rb
+++ app/controllers/vuejs_controller.rb
@@ -2,7 +2,7 @@ class VuejsController < ApplicationController
   layout false
 
   def index
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Cache-Control"] = "private, max-age=0, no-cache, no-store"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
     render file: "#{Rails.root}/vue/dist/index.html"
