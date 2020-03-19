334b2ca5b345835836b217fb9fbe3071117e84d3
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-03-18 19:33:13 -0700

    Search occupations using elasticsearch #217

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -1,33 +1,17 @@
 class Occupation < ApplicationRecord
+  searchkick
+
   validates :type, presence: true
   validates :title, uniqueness: { scope: [:rapids_code, :onet_code] }
 
   has_many :occupation_standards
   has_many :industries, through: :occupation_standards
 
-  class << self
-    def search(args={})
-      q = args[:q]
-      return all if q.blank?
-
-      terms = q.split(/\s+/)
-      condition = "(occupations.title ILIKE :term OR sub.str ILIKE :term)"
-      conditions = terms.inject([]) do |array, term|
-        array << sanitize_sql_for_conditions([condition, term: "%#{term}%"])
-      end
-      condition_str = conditions.join(" OR ")
-
-      Occupation
-        .select("occupations.*")
-        .from("occupations
-                 LEFT JOIN
-               (SELECT
-                  occupations.id,
-                  UNNEST(title_aliases) AS str
-                FROM occupations) AS sub ON (occupations.id = sub.id)")
-        .where(condition_str)
-        .distinct
-    end
+  def search_data
+    {
+      title: title,
+      title_aliases: title_aliases,
+    }
   end
 
   def title_aliases=(list)
