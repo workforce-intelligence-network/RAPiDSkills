dd24022cc33d234708f0ab639e8389b732cd3c79
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-03-24 11:24:34 -0700

    Add search method for skills #95

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,9 +1,22 @@
 class Skill < ApplicationRecord
+  searchkick
+
   belongs_to :parent_skill, class_name: 'Skill', optional: true
   has_many :occupation_standard_skills
   has_many :occupation_standards, through: :occupation_standard_skills
 
   validates :description, presence: true
+
+  class << self
+    def search_records(q:)
+      return all if q.blank?
+      search(q, operator: "or")
+    end
+  end
+
+  def search_data
+    { description: description }
+  end
 
   def to_s
     description
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -5,4 +5,43 @@ RSpec.describe Skill, type: :model do
     skill = build(:skill)
     expect(skill.valid?).to be true
   end
+
+  describe ".search_records" do
+    let!(:skill1) { create(:skill, description: "Fig Berry") }
+    let!(:skill2) { create(:skill, description: "Berry Chocolate") }
+    let!(:skill3) { create(:skill, description: "Ginger Blueberry") }
+
+    before { Skill.reindex }
+
+    it "returns all results when q is blank" do
+      results = Skill.search_records(q: nil)
+      expect(results).to contain_exactly skill1, skill2, skill3
+
+      results = Skill.search_records(q: "")
+      expect(results).to contain_exactly skill1, skill2, skill3
+    end
+
+    it "returns matched results when q is not blank" do
+      results = Skill.search_records(q: "Fig")
+      expect(results).to contain_exactly skill1
+
+      results = Skill.search_records(q: "Berry")
+      expect(results).to contain_exactly skill1, skill2
+    end
+
+    it "returns OR matched results when q has multiple terms" do
+      results = Skill.search_records(q: "Chocolate Fob")
+      expect(results).to contain_exactly skill2
+    end
+
+    it "returns no results when q does not match" do
+      results = Skill.search_records(q: "Fob")
+      expect(results).to be_empty
+    end
+
+    it "returns partial match for description" do
+      results = Skill.search_records(q: "Choco")
+      expect(results).to contain_exactly skill2
+    end
+  end
 end
