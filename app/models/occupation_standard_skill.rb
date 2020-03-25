bffdd46463e7b296031fb44676c6afc53055bd64
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-03-25 12:00:10 -0700

    Add comment to explain what is happening in search_records method #95

--- app/models/occupation_standard_skill.rb
+++ app/models/occupation_standard_skill.rb
@@ -10,6 +10,16 @@ class OccupationStandardSkill < ApplicationRecord
   delegate :description, to: :skill, prefix: true
   delegate :creator, to: :occupation_standard
 
+  # In our implementation of the {json:api} spec, the skill resource is
+  # actually an occupation_standard_skill object. So to return skill
+  # descriptions to the front-end, we need to either (1) define a new resource
+  # that returns the records from the skills table, or (2) return
+  # occupation_standard_skill records so we are consistent with other
+  # skill-related endpoints. This implementation is doing the latter. The
+  # search_records method first collects the underlying skill records for the
+  # search term provided, and then grabs the first occupation_standard_skill
+  # record that matches each skill. The UNNEST statement keeps the order of the
+  # skills that was returned from elasticsearch.
   class << self
     def search_records(q:)
       if q.blank?
